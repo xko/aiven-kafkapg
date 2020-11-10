38abe4a4e25ed478441abf76d2b6d640f9959e85
Niko Ugarov <xxkkoo@gmail.com>
2020-11-11 00:53:49 +0100

    proof-of-concept: publishing to docked kafka

--- build.sbt
+++ build.sbt
@@ -7,6 +7,7 @@ scalacOptions ++= Seq("-deprecation", "-feature")
 libraryDependencies += "com.github.oshi" % "oshi-core" % "5.3.4"
 
 libraryDependencies += "io.monix" %% "monix" % "3.3.0"
+libraryDependencies += "io.monix" %% "monix-kafka-1x" % "1.0.0-RC6"
 
 libraryDependencies += "org.scalatest" %% "scalatest" % "3.2.0" % Test
 
--- src/main/scala/aiven/kafkapg/Main.scala
+++ src/main/scala/aiven/kafkapg/Main.scala
@@ -1,20 +1,36 @@
 package aiven.kafkapg
 
 
-import scala.concurrent.duration._
-import monix.execution.Scheduler.Implicits.global
-import monix.reactive.Observable
+import java.net.InetAddress
+
 import oshi.SystemInfo
+import monix.execution.Scheduler
+import monix.reactive.Observable
+import monix.kafka._
+import org.apache.kafka.clients.producer.ProducerRecord
 
 import scala.concurrent.Await
+import scala.concurrent.duration._
 
 
 object Main {
   def main(args: Array[String]): Unit = {
-    val cpu = (new SystemInfo).getHardware.getProcessor
-    val f = Observable.interval(1.second).scan(0d,cpu.getSystemCpuLoadTicks) { (prev, i) =>
-      (cpu.getSystemCpuLoadBetweenTicks(prev._2),cpu.getSystemCpuLoadTicks )
-    }
-    Await.result(f.dump("CPU Load").foreach(_=>()), Duration.Inf)
+    val si = new SystemInfo
+    val cpu = si.getHardware.getProcessor
+    val hostName = InetAddress.getLocalHost.getHostName
+
+    val cpuLoads = Observable.interval(1.second).scan(0d,cpu.getSystemCpuLoadTicks) { case ( (_,ticksBefore), _) =>
+      (cpu.getSystemCpuLoadBetweenTicks(ticksBefore), cpu.getSystemCpuLoadTicks)
+    }.map(_._1)
+    val records = cpuLoads.map(
+      l => new ProducerRecord("test","",s"""{"username":"$hostName", "cpuLoad":"$l"}""")
+    )
+
+    val producerCfg = KafkaProducerConfig.default.copy( List("localhost:9092") )
+    implicit val scheduler: Scheduler = monix.execution.Scheduler.global
+
+    val producer = KafkaProducerSink[String,String](producerCfg, scheduler)
+    val doit = records.bufferIntrospective(1024).consumeWith(producer).runToFuture
+    Await.result(doit, Duration.Inf)
   }
 }
