2d4e7cf92aaf71ece358a2e121297a52a61929c7
Niko Ugarov <xxkkoo@gmail.com>
2020-11-13 18:29:19 +0100

    separated Publisher

--- src/main/scala/aiven/kafkapg/Main.scala
+++ src/main/scala/aiven/kafkapg/Main.scala
@@ -1,51 +1,13 @@
 package aiven.kafkapg
 
 
-import java.io.File
-import java.net.InetAddress
-
-import com.typesafe.config.ConfigFactory
-import oshi.SystemInfo
-import monix.execution.Scheduler
-import monix.reactive.Observable
-import monix.kafka._
-import org.apache.kafka.clients.producer.ProducerRecord
-
 import scala.concurrent.Await
 import scala.concurrent.duration._
 
 
 object Main {
   def main(args: Array[String]): Unit = {
-    val si = new SystemInfo
-    val cpu = si.getHardware.getProcessor
-    val hostName = InetAddress.getLocalHost.getHostName
-    val schema = """ {
-                   |    "type": "struct",
-                   |    "fields": [
-                   |        { "field": "host", "type": "string", "optional": false },
-                   |        { "field": "cpuLoad", "type": "string", "optional": false }
-                   |    ]
-                   |  }""".stripMargin
-
-    val cpuLoads = Observable.interval(3.second).scan(0d,cpu.getSystemCpuLoadTicks) { case ( (_,ticksBefore), _) =>
-      (cpu.getSystemCpuLoadBetweenTicks(ticksBefore), cpu.getSystemCpuLoadTicks)
-    }.map(_._1)
-    val records = cpuLoads.map( l => {
-        val msg =
-          s"""{"schema": $schema,
-             | "payload": {"host":"$hostName", "cpuLoad":"$l"}
-             | }""".stripMargin
-        println(msg)
-        new ProducerRecord[String,String]("os-metrics-log",null,msg)
-      }
-    )
-
-    val producerCfg = KafkaProducerConfig(ConfigFactory.parseFileAnySyntax(new File("client.properties")))
-    implicit val scheduler: Scheduler = monix.execution.Scheduler.global
-
-    val producer = KafkaProducerSink[String,String](producerCfg, scheduler)
-    val doit = records.bufferIntrospective(1024).consumeWith(producer).runToFuture
-    Await.result(doit, Duration.Inf)
+    Await.result(Publisher.publishCpuLoad, Duration.Inf)
   }
+
 }
---
+++ src/main/scala/aiven/kafkapg/Publisher.scala
@@ -0,0 +1,50 @@
+package aiven.kafkapg
+
+import java.io.File
+import java.net.InetAddress
+
+import com.typesafe.config.ConfigFactory
+import monix.execution.{CancelableFuture, Scheduler}
+import monix.kafka.{KafkaProducerConfig, KafkaProducerSink}
+import monix.reactive.Observable
+import org.apache.kafka.clients.producer.ProducerRecord
+import oshi.SystemInfo
+
+import scala.concurrent.duration._
+
+
+object Publisher {
+  def publishCpuLoad: CancelableFuture[Unit] = {
+    val si = new SystemInfo
+    val cpu = si.getHardware.getProcessor
+    val hostName = InetAddress.getLocalHost.getHostName
+    val schema =
+      """ {
+        |    "type": "struct",
+        |    "fields": [
+        |        { "field": "host", "type": "string", "optional": false },
+        |        { "field": "cpuLoad", "type": "string", "optional": false }
+        |    ]
+        |  }""".stripMargin
+
+    val cpuLoads = Observable.interval(3.second).scan(0d, cpu.getSystemCpuLoadTicks) { case ((_, ticksBefore), _) =>
+      (cpu.getSystemCpuLoadBetweenTicks(ticksBefore), cpu.getSystemCpuLoadTicks)
+    }.map(_._1)
+    val records = cpuLoads.map(l => {
+      val msg =
+        s"""{"schema": $schema,
+           | "payload": {"host":"$hostName", "cpuLoad":"$l"}
+           | }""".stripMargin
+      println(msg)
+      new ProducerRecord[String, String]("os-metrics-log", null, msg)
+    }
+    )
+
+    val producerCfg = KafkaProducerConfig(ConfigFactory.parseFileAnySyntax(new File("client.properties")))
+    implicit val scheduler: Scheduler = monix.execution.Scheduler.global
+
+    val producer = KafkaProducerSink[String, String](producerCfg, scheduler)
+    val doit = records.bufferIntrospective(1024).consumeWith(producer).runToFuture
+    doit
+  }
+}
