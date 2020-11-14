d78d29c4d308027d444d34f373029abaab1af161
Niko Ugarov <xxkkoo@gmail.com>
2020-11-14 23:18:49 +0100

    PgReader compatible with new model
    .. although doesn't use it yet

--- src/main/scala/aiven/kafkapg/PgReader.scala
+++ src/main/scala/aiven/kafkapg/PgReader.scala
@@ -1,6 +1,8 @@
 package aiven.kafkapg
 
 import java.io.File
+import java.sql.Timestamp
+import java.time.Instant
 
 import com.typesafe.config.ConfigFactory
 import slick.basic.DatabaseConfig
@@ -9,16 +11,15 @@ import slick.jdbc.PostgresProfile.api._
 import scala.concurrent.duration.Duration
 import scala.concurrent.{Await, Future}
 import scala.concurrent.ExecutionContext.Implicits.global
+import scala.util.Using
 
 object PgReader extends App {
-  val db = Database.forConfig("",ConfigFactory.parseFileAnySyntax(new File("pg.client.properties")))
-  def getEntries(limit: Int): DBIO[Seq[(String, String)]] =
-    sql"SELECT * FROM os_metrics limit $limit".as[(String,String)]
-
-  try {
+  def getEntries(limit: Int): DBIO[Seq[(String, Double, Timestamp)]] =
+    sql"SELECT * FROM os_metrics limit $limit".as[(String, Double, Timestamp)]
+  Using( Database.forConfig("",ConfigFactory.parseFileAnySyntax(new File("pg.client.properties"))) ){ db =>
     val a: DBIO[Unit] = getEntries(50).map(_.foreach(println))
     val f: Future[Unit] = db.run(a)
     Await.result(f, Duration.Inf)
-  } finally db.close
+  }
 
 }
