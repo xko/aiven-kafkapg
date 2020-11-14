526dc0ae47d1288fd6f10426819053660e26216c
Niko Ugarov <xxkkoo@gmail.com>
2020-11-15 00:19:33 +0100

    PgReader actually uses the model class
    opens the full power of Slick!

--- src/main/scala/aiven/kafkapg/PgReader.scala
+++ src/main/scala/aiven/kafkapg/PgReader.scala
@@ -5,21 +5,24 @@ import java.sql.Timestamp
 import java.time.Instant
 
 import com.typesafe.config.ConfigFactory
-import slick.basic.DatabaseConfig
 import slick.jdbc.PostgresProfile.api._
 
-import scala.concurrent.duration.Duration
-import scala.concurrent.{Await, Future}
+import scala.concurrent.Await
 import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration.Duration
 import scala.util.Using
 
 object PgReader extends App {
-  def getEntries(limit: Int): DBIO[Seq[(String, Double, Timestamp)]] =
-    sql"SELECT * FROM os_metrics limit $limit".as[(String, Double, Timestamp)]
   Using( Database.forConfig("",ConfigFactory.parseFileAnySyntax(new File("pg.client.properties"))) ){ db =>
-    val a: DBIO[Unit] = getEntries(50).map(_.foreach(println))
-    val f: Future[Unit] = db.run(a)
-    Await.result(f, Duration.Inf)
+    val a = TableQuery[OsMetricsTable].take(50).result.map(_.foreach(println))
+    Await.result(db.run(a), Duration.Inf)
   }
-
 }
+
+class OsMetricsTable(tag: Tag) extends Table[OsMetrics](tag, "os_metrics") {
+  def hostName = column[String]("hostName")
+  def timestamp = column[Instant]("timestamp")
+  def cpuLoad = column[Double]("cpuLoad")
+  def * = (timestamp, hostName, cpuLoad) <> ((OsMetrics.apply _).tupled, OsMetrics.unapply)
+}
+
