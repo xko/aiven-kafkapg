9ee1cfa18165693bc9fd1e626baa9b51cbfe4c82
Niko Ugarov <xxkkoo@gmail.com>
2020-11-16 03:58:39 +0100

    better postgres examples

--- src/main/scala/aiven/kafkapg/PgReader.scala
+++ src/main/scala/aiven/kafkapg/PgReader.scala
@@ -2,8 +2,10 @@ package aiven.kafkapg
 
 import java.io.File
 import java.time.Instant
+import java.time.temporal.{ChronoUnit, TemporalUnit}
 
 import com.typesafe.config.ConfigFactory
+import slick.dbio.NoStream
 import slick.jdbc.PostgresProfile.api._
 
 import scala.concurrent.Await
@@ -11,14 +13,34 @@ import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration.Duration
 import scala.util.{Failure, Success, Using}
 
-object PgReader extends App {
-  Using( Database.forConfig("",ConfigFactory.parseFileAnySyntax(new File(".pg/client.properties"))) ){ db =>
-    val a = TableQuery[OsMetricsTable].sortBy(_.timestamp.desc).take(10).result
+
+object PgReader  {
+  def inDb[R](a:DBIOAction[R, NoStream, Nothing])(onRes: R=>Unit) = Using( Database.forConfig("",ConfigFactory.parseFileAnySyntax(new File(".pg/client.properties"))) ){ db =>
     val res = Await.ready(db.run(a), Duration.Inf).value.get
     res match {
-      case Success(recs) => println(recs.mkString("\n"))
+      case Success(r) => onRes(r)
       case Failure(err) => err.printStackTrace()
     }
+  }
+
+  def queryByHostOrAll(host: Option[String]): Query[OsMetricsTable, OsMetrics, Seq] = host.fold(TableQuery[OsMetricsTable].sortBy(_.timestamp.desc)) { host =>
+    TableQuery[OsMetricsTable].filter(_.hostName === host)
+  }
+
+}
+
+import PgReader._
+
+object Last10From extends App {
+  inDb(queryByHostOrAll(args.headOption).take(10).result){ r =>
+    println(r.mkString("\n"))
+  }
+}
+
+object AvgCPULastHour extends App {
+  inDb(queryByHostOrAll(args.headOption).filter(_.timestamp > Instant.now().minus(1, ChronoUnit.HOURS))
+         .map(_.cpuLoad).avg.result){ r =>
+    println(r)
   }
 }
 
