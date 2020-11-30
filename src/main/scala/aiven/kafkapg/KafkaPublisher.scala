4df2d4f1f642d923b35e251c57b788d60de47e97
Niko Ugarov <xxkkoo@gmail.com>
2020-11-30 23:52:00 +0100

    KafkaPublisher generalized
    can now send anything with any schedule, config can be overridden

--- src/main/scala/aiven/kafkapg/KafkaPublisher.scala
+++ src/main/scala/aiven/kafkapg/KafkaPublisher.scala
@@ -1,38 +1,41 @@
 package aiven.kafkapg
 
-import java.io.File
-
 import com.typesafe.config.ConfigFactory
 import io.github.azhur.kafkaserdejson4s.Json4sSupport
+import monix.eval.Task
 import monix.execution.Scheduler
 import monix.kafka.{KafkaProducerConfig, KafkaProducerSink}
 import monix.reactive.Observable
 import org.apache.kafka.clients.producer.ProducerRecord
 import org.json4s._
 
+import java.io.File
 import scala.concurrent.Await
 import scala.concurrent.duration._
-import scala.util.Failure
 
 
-object KafkaPublisher extends App with Json4sSupport {
-  implicit val fmt: Formats = Codecs.formats + Codecs.withSchema[OsMetrics]
-  implicit val ser: Serialization = org.json4s.jackson.Serialization
+object KafkaPublisher extends Json4sSupport {
+  lazy val defaultConfig: KafkaProducerConfig =
+    KafkaProducerConfig( ConfigFactory.parseFileAnySyntax(new File(".kafka/client.properties")) )
 
-  val metrics = Observable.interval(3.second).scan(OsMetrics.initial)( (metrics, _) => metrics.next )
-  val records = metrics.map(
-    new ProducerRecord[String, OsMetrics]("os_metrics", null, _)
-  )
+  val defaultTopic = "os_metrics"
 
-  val producerCfg = KafkaProducerConfig(ConfigFactory.parseFileAnySyntax(new File(".kafka/client.properties")))
   implicit val scheduler: Scheduler = monix.execution.Scheduler.global
 
-  val producer = KafkaProducerSink[String,OsMetrics](producerCfg, scheduler)
-  val doit = records.bufferIntrospective(1024).consumeWith(producer).runToFuture
-  val res = Await.ready(doit, Duration.Inf).value.get
-  res match {
-    case Failure(err) => err.printStackTrace()
-    case _ =>
+  def publish[T <:AnyRef : Schematic](items: Observable[T], topic:String = defaultTopic,
+                                      config: KafkaProducerConfig = defaultConfig): Task[Unit] = {
+    implicit val fmt: Formats = Codecs.formats + Codecs.withSchema[T]
+    implicit val ser: Serialization = org.json4s.jackson.Serialization
+
+    val records = items.map( new ProducerRecord[String, T](topic, null, _) )
+    val producer = KafkaProducerSink[String,T](config, scheduler)
+    records.bufferIntrospective(1024).consumeWith(producer)
+  }
+
+  def main(args: Array[String]): Unit = {
+    val go = publish(Observable.interval(3.second).scan(OsMetrics.initial)((metrics, _) => metrics.next))
+             .runToFuture
+    Await.ready(go, Duration.Inf).value.get.get
   }
 
 }
