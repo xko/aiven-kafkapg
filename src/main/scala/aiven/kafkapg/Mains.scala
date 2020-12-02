881c2158a4f9ae9816628f09d7a3b1f31cce0f11
Niko Ugarov <xxkkoo@gmail.com>
2020-12-02 22:45:31 +0100

    awaiting only in one place, everything else is pure Tasks

--- src/main/scala/aiven/kafkapg/Mains.scala
+++ src/main/scala/aiven/kafkapg/Mains.scala
@@ -1,40 +1,42 @@
 package aiven.kafkapg
 
-import PgReader._
+import aiven.kafkapg.PgReader._
+import monix.eval.Task
+import monix.execution.Scheduler.Implicits.global
 import monix.reactive.Observable
+import slick.jdbc.PostgresProfile.api._
 
 import java.time.Instant
 import java.time.temporal.ChronoUnit
-import slick.jdbc.PostgresProfile.api._
-
-import scala.concurrent.{Await, Future}
+import scala.concurrent.Await
 import scala.concurrent.duration._
-import monix.execution.Scheduler.Implicits.global
 
-trait MainCanWait {
+trait MainCanWait { //TODO: Why didn't monix.eval.TaskApp work?
   def main(args: Array[String]): Unit = {
-    Await.ready(go(args), Duration.Inf).value.get.get
+    Await.result(go(args).runToFuture, Duration.Inf)
   }
 
-  def go(args: Array[String]) : Future[Unit]
+  def go(args: Array[String]): Task[Unit]
 }
 
 object FromPgLast10Records extends MainCanWait {
-  def go(args:Array[String]): Future[Unit] = inDb()(_.run(OsMetricsTable.query(args.headOption).take(10).result))
-                                             .map(r => println(r.mkString("\n")))
+  override def go(args: Array[String]): Task[Unit] =
+    inDb()(runQ( OsMetricsTable.query(args.headOption).take(10).result ))
+      .map(_.mkString("\n")).map(println)
 }
 
 object FromPgAvgCPULastHour extends MainCanWait {
-  override def go(args: Array[String]): Future[Unit] = inDb()(_.run(
-    OsMetricsTable.query(args.headOption)
-                  .filter(_.timestamp > Instant.now().minus(1, ChronoUnit.HOURS))
-                  .map(_.cpuLoad).avg.result
-  )).map(println)
+  override def go(args: Array[String]): Task[Unit] =
+    inDb()(runQ(
+      OsMetricsTable.query(args.headOption)
+                    .filter(_.timestamp > Instant.now().minus(1, ChronoUnit.HOURS))
+                    .map(_.cpuLoad).avg.result
+    )) .map(println)
 }
 
 object ToKafkaConnectEvery3s extends MainCanWait {
-  override def go(args: Array[String]): Future[Unit] =
+  override def go(args: Array[String]): Task[Unit] =
     KafkaPublisher.publish4KConnect(
       Observable.interval(3.second).scan(OsMetrics.initial)( (metrics, _) => metrics.next )
-    ).runToFuture
+    )
 }
--- src/main/scala/aiven/kafkapg/PgReader.scala
+++ src/main/scala/aiven/kafkapg/PgReader.scala
@@ -1,18 +1,19 @@
 package aiven.kafkapg
 
 import com.typesafe.config.{Config, ConfigFactory}
+import monix.eval.Task
 import slick.jdbc.PostgresProfile.api._
 
 import java.io.File
-import scala.util.Using
 
 
 object PgReader  {
   val defaultConfig: Config = ConfigFactory.parseFileAnySyntax(new File(".pg/client.properties"))
 
-  def inDb[R](config:Config = defaultConfig)(action: Database => R):R =
-    Using.resource( Database.forConfig("", config) )(action)
+  def inDb[R](config:Config = defaultConfig)(action: Database => Task[R]): Task[R] =
+    Task.eval( Database.forConfig("",config) ).bracket(action)( db => Task(db.close()) )
 
+  def runQ[R](thunk:DBIO[R])(db: Database) :Task[R] = Task.fromFuture(db.run(thunk))
 }
 
 
