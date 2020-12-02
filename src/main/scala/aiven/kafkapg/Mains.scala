fc697ee13a6af90328561a186c412899352b9a88
Niko Ugarov <xxkkoo@gmail.com>
2020-12-02 16:10:30 +0100

    awaiting only in Mains now; PgReader generalized

--- src/main/scala/aiven/kafkapg/Mains.scala
+++ src/main/scala/aiven/kafkapg/Mains.scala
@@ -7,27 +7,34 @@ import java.time.Instant
 import java.time.temporal.ChronoUnit
 import slick.jdbc.PostgresProfile.api._
 
-import scala.concurrent.Await
+import scala.concurrent.{Await, Future}
 import scala.concurrent.duration._
-
 import monix.execution.Scheduler.Implicits.global
 
-object FromPgLast10Records extends App {
-  inDb(OsMetricsTable.query(args.headOption).take(10).result){ r =>
-    println(r.mkString("\n"))
+trait MainCanWait {
+  def main(args: Array[String]): Unit = {
+    Await.ready(go(args), Duration.Inf).value.get.get
   }
+
+  def go(args: Array[String]) : Future[Unit]
 }
 
-object FromPgAvgCPULastHour extends App {
-  inDb( OsMetricsTable.query(args.headOption)
-          .filter(_.timestamp > Instant.now().minus(1, ChronoUnit.HOURS))
-          .map(_.cpuLoad).avg.result ) { r =>
-    println(r)
-  }
+object FromPgLast10Records extends MainCanWait {
+  def go(args:Array[String]): Future[Unit] = inDb()(_.run(OsMetricsTable.query(args.headOption).take(10).result))
+                                             .map(r => println(r.mkString("\n")))
 }
 
-object ToKafkaConnectEvery3s extends App {
-  val go = KafkaPublisher.publish4KConnect( Observable.interval(3.second).scan(OsMetrics.initial )
-                                          ( (metrics, _) => metrics.next) ).runToFuture
-  Await.ready(go, Duration.Inf).value.get.get
+object FromPgAvgCPULastHour extends MainCanWait {
+  override def go(args: Array[String]): Future[Unit] = inDb()(_.run(
+    OsMetricsTable.query(args.headOption)
+                  .filter(_.timestamp > Instant.now().minus(1, ChronoUnit.HOURS))
+                  .map(_.cpuLoad).avg.result
+  )).map(println)
+}
+
+object ToKafkaConnectEvery3s extends MainCanWait {
+  override def go(args: Array[String]): Future[Unit] =
+    KafkaPublisher.publish4KConnect(
+      Observable.interval(3.second).scan(OsMetrics.initial)( (metrics, _) => metrics.next )
+    ).runToFuture
 }
--- src/main/scala/aiven/kafkapg/PgReader.scala
+++ src/main/scala/aiven/kafkapg/PgReader.scala
@@ -1,28 +1,17 @@
 package aiven.kafkapg
 
-import java.io.File
-import java.time.Instant
-import java.time.temporal.{ChronoUnit, TemporalUnit}
-
-import com.typesafe.config.ConfigFactory
-import slick.dbio.NoStream
+import com.typesafe.config.{Config, ConfigFactory}
 import slick.jdbc.PostgresProfile.api._
 
-import scala.concurrent.Await
-import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.duration.Duration
-import scala.util.{Failure, Success, Using}
+import java.io.File
+import scala.util.Using
 
 
 object PgReader  {
-  def inDb[R](a:DBIOAction[R, NoStream, Nothing])(onRes: R=>Unit) =
-    Using( Database.forConfig("",ConfigFactory.parseFileAnySyntax(new File(".pg/client.properties"))) ){ db =>
-      val res = Await.ready(db.run(a), Duration.Inf).value.get
-      res match {
-        case Success(r) => onRes(r)
-        case Failure(err) => err.printStackTrace()
-      }
-    }
+  val defaultConfig: Config = ConfigFactory.parseFileAnySyntax(new File(".pg/client.properties"))
+
+  def inDb[R](config:Config = defaultConfig)(action: Database => R):R =
+    Using.resource( Database.forConfig("", config) )(action)
 
 }
 
