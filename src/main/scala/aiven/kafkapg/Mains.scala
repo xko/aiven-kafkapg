d2ffcb84efe7a48c9fb9fb91bf3692db549572b1
Niko Ugarov <xxkkoo@gmail.com>
2020-12-05 01:46:15 +0100

    can now bracket Observable in DB

--- src/main/scala/aiven/kafkapg/Mains.scala
+++ src/main/scala/aiven/kafkapg/Mains.scala
@@ -4,6 +4,7 @@ import aiven.kafkapg.Postgres._
 import monix.eval.Task
 import monix.execution.Scheduler.Implicits.global
 import monix.reactive.Observable
+import org.json4s.Formats
 import slick.jdbc.PostgresProfile.api._
 
 import java.time.Instant
@@ -26,13 +27,13 @@ trait MainCanWait { //TODO: Why didn't monix.eval.TaskApp work?
 
 object FromPgLast10Records extends MainCanWait {
   override def go(args: Array[String]): Task[Unit] =
-    inDb()(runQ(OsMetricsTable.queryBy(args.headOption).take(10).result))
+    taskInDb()(runQ(OsMetricsTable.queryBy(args.headOption).take(10).result))
       .map(_.mkString("\n")).map(println)
 }
 
 object FromPgAvgCPULastHour extends MainCanWait {
   override def go(args: Array[String]): Task[Unit] =
-    inDb()(runQ(
+    taskInDb()(runQ(
       OsMetricsTable.queryBy(args.headOption)
                     .filter(_.timestamp > Instant.now().minus(1, ChronoUnit.HOURS))
                     .map(_.cpuLoad).avg.result
@@ -48,7 +49,7 @@ object ToKafkaConnectEvery3s extends MainCanWait {
 }
 
 object ToKafkaEvery3s extends MainCanWait {
-  implicit val fmt = Json.formats
+  implicit val fmt: Formats = Json.formats
   override def go(args: Array[String]): Task[Unit] =
     KafkaPublisher.publish(
       Observable.interval(3.second).scan(OsMetrics.initial)( (metrics, _) => metrics.next ),
--- src/main/scala/aiven/kafkapg/Postgres.scala
+++ src/main/scala/aiven/kafkapg/Postgres.scala
@@ -2,6 +2,7 @@ package aiven.kafkapg
 
 import com.typesafe.config.{Config, ConfigFactory}
 import monix.eval.Task
+import monix.reactive.Observable
 import slick.jdbc.PostgresProfile.api._
 
 import java.io.File
@@ -10,10 +11,14 @@ import java.io.File
 object Postgres  {
   val defaultConfig: Config = ConfigFactory.parseFileAnySyntax(new File(".pg/client.properties"))
 
-  def inDb[R](config:Config = defaultConfig)(action: Database => Task[R]): Task[R] =
+  def taskInDb[R](config:Config = defaultConfig)(action: Database => Task[R]): Task[R] =
     Task.eval( Database.forConfig("",config) ).bracket(action)( db => Task(db.close()) )
 
-  def runQ[R](thunk:DBIO[R])(db: Database) :Task[R] = Task.fromFuture(db.run(thunk))
+  def streamInDb[R](config:Config = defaultConfig)(action: Database => Observable[R]): Observable[R] = {
+    Observable.eval( Database.forConfig("",config) ).bracket(action)( db => Task(db.close()) )
+  }
+
+  def runQ[R](thunk:DBIO[R])(db: Database) :Task[R] = Task.deferFuture(db.run(thunk))
 }
 
 
