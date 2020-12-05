e7d58abcf726fc24dba58f448eb6cee4139b9977
Niko Ugarov <xxkkoo@gmail.com>
2020-12-05 01:52:50 +0100

    migrated ITs to AsyncSpec - no more Awaits

--- src/it/scala/aiven/kafkapg/KafkaConnectIntegration.scala
+++ src/it/scala/aiven/kafkapg/KafkaConnectIntegration.scala
@@ -3,27 +3,21 @@ package aiven.kafkapg
 import aiven.kafkapg.Postgres._
 import monix.execution.Scheduler.Implicits.global
 import monix.reactive.Observable
-import org.scalatest.concurrent.{Eventually, IntegrationPatience}
-import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.flatspec.AsyncFlatSpec
 import org.scalatest.matchers.should.Matchers
 import slick.jdbc.PostgresProfile.api._
 
 import java.time.temporal.ChronoUnit
-import scala.concurrent.Await
-import scala.concurrent.duration._
 import scala.util.Random
 
-class KafkaConnectIntegration extends AnyFlatSpec with Matchers with Eventually with IntegrationPatience {
+class KafkaConnectIntegration extends AsyncFlatSpec with Matchers  {
   it should "deliver to postgres" in {
     val metrics = OsMetrics.initial.copy(hostName = "machine-" + Random.nextInt(100000))
     val publish = KafkaPublisher.publish4KConnect(Observable.eval(metrics), OsMetrics.topic4KafkaConnect)
-    Await.result(publish.runToFuture, 10.seconds)
     val readPg = taskInDb()(runQ(OsMetricsTable.queryBy(None).filter(_.hostName === metrics.hostName).result))
-    eventually {
-      val res = Await.result(readPg.runToFuture, 10.seconds)
-      val expected = metrics.copy(timestamp = metrics.timestamp.truncatedTo(ChronoUnit.MILLIS)) //pg doesn't keep nanos
-      res should contain(expected)
-    }
+    val expected = metrics.copy(timestamp = metrics.timestamp.truncatedTo(ChronoUnit.MILLIS)) //pg doesn't keep nanos
+    val verify = readPg.map(_.head should === (expected) ).onErrorRestart(10)
+    publish.flatMap(_ => verify).runToFuture
   }
 
 }
